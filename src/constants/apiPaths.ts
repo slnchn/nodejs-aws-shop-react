1872e36aa742701d592ecd24c8c12d410155edf4
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-01-01 20:07:32 +0100

    update api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://3da8zh16e9.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "https://ejbywu1m1l.execute-api.eu-west-1.amazonaws.com/prod/api",
+  order: "http://slnchn-cart-api-prod-3.eu-west-1.elasticbeanstalk.com/api",
   // order: "http://localhost:4000/api",
   import: "https://pazp3i79pa.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://ejbywu1m1l.execute-api.eu-west-1.amazonaws.com/prod/api",
+  cart: "http://slnchn-cart-api-prod-3.eu-west-1.elasticbeanstalk.com/api",
   // cart: "http://localhost:4000/api",
 };
 
