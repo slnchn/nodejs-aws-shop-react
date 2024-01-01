196f5b9a4784e8892a537e776055280759aa8e37
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-01-01 21:02:37 +0100

    oops

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://3da8zh16e9.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "https://nvkgb2j9q0.execute-api.eu-west-1.amazonaws.com/api/order",
+  order: "https://nvkgb2j9q0.execute-api.eu-west-1.amazonaws.com/api",
   // order: "http://localhost:4000/api",
   import: "https://pazp3i79pa.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://nvkgb2j9q0.execute-api.eu-west-1.amazonaws.com/api/order",
+  cart: "https://nvkgb2j9q0.execute-api.eu-west-1.amazonaws.com/api",
   // cart: "http://localhost:4000/api",
 };
 
