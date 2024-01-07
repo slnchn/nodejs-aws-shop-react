42828aca2816ec67a49520c93f541453634f6a47
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-01-07 17:50:13 +0100

    fix urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
+  product: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com",
   order: "https://nvkgb2j9q0.execute-api.eu-west-1.amazonaws.com/api",
   import: "https://pazp3i79pa.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
-  cart: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
+  bff: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com",
+  cart: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
