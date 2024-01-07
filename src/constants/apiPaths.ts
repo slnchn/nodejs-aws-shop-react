8e447432b2d44485ece5218cd29dc7b64e0edf39
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-01-07 14:46:18 +0100

    oops, update the order route back

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
-  order: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
+  order: "https://nvkgb2j9q0.execute-api.eu-west-1.amazonaws.com/api",
   import: "https://pazp3i79pa.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
   cart: "https://hqq9gzintd.execute-api.eu-west-1.amazonaws.com/",
